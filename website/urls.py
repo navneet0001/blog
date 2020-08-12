2e3ab85ebd1f8c79ea5d86477f70bc0cefe740ec
Navneet <navneet91159@gmail.com>
2020-08-12 18:46:46 +0530

    second commit

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
    path('',views.index, name="index"),
+   path('index.html',views.index, name="index"),
+   path('contact.html', views.contact, name="contact"),
+   path('blog-single.html', views.blog_single, name="blog-single"),
+   path('blog.html', views.blog, name="blog"),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,12 @@ from . import views
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {})  
+
+def blog_single(request):
+    return render(request, 'blog-single.html', {}) 
+
+def blog(request):
+    return render(request, 'blog.html', {})           
